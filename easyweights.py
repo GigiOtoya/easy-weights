ae7bb80a097b0a90fc31f06adb5ecd9ae6b6ffc4
Luigi Otoya <Luigi.Otoya@gmail.com>
2024-05-31 21:51:03 -0400

    targets property group and operator setup

--- easyweights.py
+++ easyweights.py
@@ -3,27 +3,25 @@ from typing import List
 from bpy.types import Context, Object, Collection
 
 class EasyWeightsProperty(bpy.types.PropertyGroup):
-    _TargetType = Collection | List[Object]
-
     # Mesh only selection function for poll property
     def poll_mesh(self, object: Object):
         return object.type == 'MESH'
     
-    def poll_collection(self, objects: _TargetType):
-        return 
+    def poll_collection(self, collection:Collection):
+        return any(obj.type == 'MESH' for obj in collection.objects)
     
     # body: bpy.types.Object
-    body: bpy.props.PointerProperty(
-        name="Source Mesh",
+    BODY: bpy.props.PointerProperty(
+        name="Source",
         description="Select the source mesh from which weights will be transferred from",
         type=Object,
         poll=poll_mesh
     )
 
-    targets: bpy.props.PointerProperty(
-        name="Collection",
+    TARGETS: bpy.props.PointerProperty(
+        name="Targets",
         description="Objects that will have weights transferred to",
-        type=_TargetType,
+        type=Collection,
         poll=poll_collection
     )
 
@@ -32,17 +30,31 @@ class TransferWeightOperator(bpy.types.Operator):
     bl_label = "Weight Transfer"
 
     def execute(self, context: Context):
+        ew_properties: EasyWeightsProperty = context.scene.EasyWeightsProperty
+        
         selected_objects = context.selected_objects
         
-        source = selected_objects[-1]
-        targets = selected_objects[:len(selected_objects)-1]
+        source: Object = ew_properties.BODY
+        targets: Collection = ew_properties.TARGETS
 
-        for obj in targets:
-            bpy.ops.object.data_transfer(data_type="VGROUP_WEIGHTS", layers_select_src="NAME", layers_select_dst="ALL")
+        # source = selected_objects[-1]
+        # targets = selected_objects[:len(selected_objects)-1]
 
-        message: str = "List[objects]: " + str(selected_objects)
+        # for obj in targets:
+        #     bpy.ops.object.data_transfer(data_type="VGROUP_WEIGHTS", layers_select_src="NAME", layers_select_dst="ALL")
 
-        self.report({"INFO"}, message=message)
+        # message: str = "List[objects]: " + str(selected_objects)
+
+        for obj in targets.objects:
+            if obj.type == 'MESH' and obj != source:
+                message: str = f"Transferring weights from {source.name} to {obj.name}"
+            elif obj == source:
+                message: str = f"Skipping source mesh: {source.name}"
+            else:
+                message: str = f"Skipping non-mesh object: {obj.name}"
+                
+            self.report({"INFO"}, message=message)
+            
         return {"FINISHED"}
     
 class CleanUpOperator(bpy.types.Operator):
@@ -87,14 +99,16 @@ class EasyWeightPanel(MainPanel, bpy.types.Panel):
         ew_properties: EasyWeightsProperty = context.scene.EasyWeightsProperty
 
         section = layout.box()
-        
         row = section.row()
         row.label(text="Settings", icon='MODIFIER')
 
         row = section.row()
-        # row.label(text="Source Mesh: ")
-        row.prop(ew_properties, 'body')
-        row.box().propa()
+        row.prop(ew_properties, 'BODY')
+
+        row = section.row()
+        row.prop(ew_properties, 'TARGETS')
+
+        
 
         selected_objects = context.selected_objects
 
